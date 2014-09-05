f01d48d40272cd133b906c3ef467b60c1af7d497
Facundo Soria <facundo.soria@devecoop.com>
2014-09-05 11:42:09 -0300

    refactoring

--- lib/odf-report/images.rb
+++ lib/odf-report/images.rb
@@ -86,6 +86,8 @@ module ODFReport
       @images_to_delete.each{ |image_name|
 
         content.xpath("//draw:frame[@draw:name='#{image_name}']/draw:image/@xlink:href").each{ |image|
+          puts image
+          puts image.content
           image.content = ' '
         }
 
