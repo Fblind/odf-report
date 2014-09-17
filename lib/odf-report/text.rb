f8659889e3f5b63c6352cf02dc80c947dbe9af2a
Facundo Soria <facundo.soria@devecoop.com>
2014-09-17 15:55:57 -0300

    refactor

--- lib/odf-report/text.rb
+++ lib/odf-report/text.rb
@@ -21,7 +21,9 @@ module ODFReport
     end
 
     def self.delete_extra_text(doc, texts_to_delete)
+      puts "Los texts del xml= #{texts_to_delete}"
       texts_to_delete.each{ |text|
+        puts "#{doc.xpath(".//text:p[text()='[#{text.to_s.upcase}]']")}"
         doc.xpath(".//text:p[text()='[#{text.to_s.upcase}]']").remove
       }
       
